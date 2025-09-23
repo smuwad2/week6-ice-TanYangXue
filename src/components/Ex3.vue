83a70324ea38be40597ff1e7babeb6dd1a1eb621
TanYangXue <tanyangxue@gmail.com>
2025-09-23 02:59:48 +0000

    Exercise 3 completed

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -2,22 +2,31 @@
     export default { 
 
        // add code here
-
+       data() {
+        return {
+            loginField: "username"
+        }
     }
+}
 </script>
 
 <template>
     <h1>Please select your login option</h1>
     <!-- Add/modify code in the following to have a dropdown list 
         which lets the user to select either to login with username or email -->
-    <select>
-        <option selected>Username login</option>
-        <option>Email login</option>
+
+    <select v-model="loginField">
+        <option value="username" selected>Username login</option>
+        <option value="email">Email login</option>
     </select>
     
-    <div>
+    <div v-if="loginField == 'username'">
         <label>Username</label>
-        <input placeholder="Enter your username">
+        <input placeholder="Enter your Username">
+    </div>
+    <div v-else>
+        <label>Username</label>
+        <input placeholder="Enter your Email">
     </div>
 </template>
 
