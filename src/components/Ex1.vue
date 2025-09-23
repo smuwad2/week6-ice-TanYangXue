52ddf43a798f127436dceceb3b26aa1b0239fe0e
TanYangXue <tanyangxue@gmail.com>
2025-09-23 01:26:45 +0000

    Finished Exercise 1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,7 +1,16 @@
 <script>
     export default { 
-        // Add Code Here
-        // challenge: use array  
+        data () {
+            return {
+                message: "Choose your favourite fruit: ",
+                fruit1: "apple",
+                fruit2: "orange",
+                fruit3: "peach",
+            }
+        },
+        methods: {
+
+        }  
     }
 </script>
 
